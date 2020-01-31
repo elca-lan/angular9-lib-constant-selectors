8b5769aa5d47f8234e6a54131f58bae0eba37be7
Lukas Angerer <lukas.angerer@elca.ch>
2020-01-31 11:10:47 +0100

    Added second library with "ng generate library business"

---
+++ projects/business/src/lib/business.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { BusinessComponent } from './business.component';
+
+
+
+@NgModule({
+  declarations: [BusinessComponent],
+  imports: [
+  ],
+  exports: [BusinessComponent]
+})
+export class BusinessModule { }
---
+++ projects/business/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of business
+ */
+
+export * from './lib/business.service';
+export * from './lib/business.component';
+export * from './lib/business.module';
